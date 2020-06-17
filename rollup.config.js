28ba79fca031d61bd18356fdc0bdb063336d02a6
vectronic <hello.vectronic@gmail.com>
2020-06-17 22:59:30 +0100

    feat: initial empty codebase

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,6 @@
-import builtins from 'rollup-plugin-node-builtins';
 import cleanup from 'rollup-plugin-cleanup';
 import commonjs from 'rollup-plugin-commonjs';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-import resolve from 'rollup-plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript2';
 import ts from 'typescript';
 import tempDir from 'temp-dir';
@@ -10,38 +8,7 @@ import tempDir from 'temp-dir';
 module.exports = [
     {
         input: {
-            node: 'src/index.ts'
-        },
-        output: {
-            dir: 'dist',
-            format: 'es',
-            sourcemap: true
-        },
-        watch: {
-            include: 'src/**',
-        },
-        external: [
-            'path',
-            'fs',
-            'crypto'
-        ],
-        plugins: [
-            peerDepsExternal(),
-            typescript({
-                typescript: ts,
-                useTsconfigDeclarationDir: true,
-                cacheRoot: `${tempDir}/.rpt2_cache`
-            }),
-            commonjs(),
-            resolve(),
-            cleanup({
-                extensions: ['ts']
-            })
-        ]
-    },
-    {
-        input: {
-            browser: 'src/index.ts'
+            api: 'src/index.ts'
         },
         output: {
             dir: 'dist',
@@ -59,11 +26,6 @@ module.exports = [
                 cacheRoot: `${tempDir}/.rpt2_cache`
             }),
             commonjs(),
-            builtins(),
-            resolve({
-                browser: true,
-                preferBuiltins: false
-            }),
             cleanup({
                 extensions: ['ts']
             })
